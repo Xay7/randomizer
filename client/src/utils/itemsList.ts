973a85427d4a5c9803b361988cc1b1ef2c2f1376
Emil <emilian.cw@gmail.com>
2019-08-06 11:46:16 +0200

    Boilerplate data for categories with items

---
+++ client/src/utils/itemsList.ts
@@ -0,0 +1,12 @@
+export const data: { [key: string]: Array<{ name: string, icon: string }> } = {
+    entertainment: [
+        {
+            name: "Movie",
+            icon: "film"
+        },
+        {
+            name: "Game",
+            icon: "gamepad"
+        }
+    ]
+}
